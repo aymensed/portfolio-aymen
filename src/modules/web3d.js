b30c49000958a9daf2c9dedf8a84f6822be9898e
aymensed <aymensedjal19@gmail.com>
2024-10-26 20:43:36 +0200

    Ajout de mes modifications

--- src/modules/web3d.js
+++ src/modules/web3d.js
@@ -1,53 +1,66 @@
-import React from 'react';
-import './web3d.css'; // Fichier CSS pour les styles
-import tp1ImageOld from './chaise.png'; // Ancienne version de l'image
-import tp1ImageNew1 from './frame14.png'; // Nouvelle version améliorée 1
-import tp1ImageNew2 from './frame11.png'; // Nouvelle version améliorée 2
-import tp1ImageNew3 from './frame13.png'; // Nouvelle version améliorée 3
-import tp1ImageNew4 from './frame12.png'; // Nouvelle version améliorée 3
+import React, { useState } from 'react';
+import './web3d.css'; // Assurez-vous d'avoir un fichier CSS stylisé
+import tp1ImageOld from './chaise.png';
+import tp1ImageNew1 from './frame14.png';
+import tp1ImageNew2 from './frame11.png';
+import tp1ImageNew3 from './frame13.png';
+import tp1ImageNew4 from './frame12.png';
+
+const images = [tp1ImageOld, tp1ImageNew1, tp1ImageNew2, tp1ImageNew3, tp1ImageNew4];
 
 const Web3D = () => {
+    const [currentImage, setCurrentImage] = useState(0);
+
+    const handlePrev = () => {
+        setCurrentImage((currentImage - 1 + images.length) % images.length);
+    };
+
+    const handleNext = () => {
+        setCurrentImage((currentImage + 1) % images.length);
+    };
+
+    const downloadImage = () => {
+        const link = document.createElement("a");
+        link.href = images[currentImage];
+        link.download = `image${currentImage + 1}.png`;
+        link.click();
+    };
+
     return ( <
+        >
+        <
+        div className = "header-module" >
+        <
+        h1 > Module Web3D < /h1> <
+        /div>
+
+        <
         div className = "web3d-container" >
         <
-        h1 > Module Web 3 D < /h1> <
         h2 > TP1: Création d 'une scène avec Blender</h2> <
         p >
         L 'objectif de ce TP est de créer une scène avec Blender. Voici les différentes versions de la scène de la chaise : <
         /p>
 
-        { /* Affichage de la version ancienne de la scène */ } <
-        div className = "image-container" >
         <
-        h3 > Version Ancienne < /h3> <
-        img src = { tp1ImageOld }
-        alt = "Ancienne scène créée avec Blender"
+        div className = "carousel-container" >
+        <
+        button onClick = { handlePrev }
+        className = "nav-button prev" > ❮ < /button> <
+        div className = "image-frame" >
+        <
+        img src = { images[currentImage] }
+        alt = { `Image ${currentImage + 1}` }
         className = "tp-image" / >
         <
-        /div>
-
-        { /* Affichage des versions améliorées de la scène */ } <
-        div className = "image-container" >
-        <
-        h3 > Versions Améliorées < /h3> <
-        img src = { tp1ImageNew1 }
-        alt = "Scène améliorée 1"
-        className = "tp-image" / >
-        <
-        img src = { tp1ImageNew2 }
-        alt = "Scène améliorée 2"
-        className = "tp-image" / >
-        <
-        img src = { tp1ImageNew3 }
-        alt = "Scène améliorée 3"
-        className = "tp-image" / >
-        <
-        img src = { tp1ImageNew4 }
-        alt = "Scène améliorée 4"
-        className = "tp-image" / >
-        <
+        button onClick = { downloadImage }
+        className = "download-button" > Télécharger l 'image</button> <
         /div> <
-        /div>
+        button onClick = { handleNext }
+        className = "nav-button next" > ❯ < /button> <
+        /div> <
+        /div> <
+        />
     );
 };
 
